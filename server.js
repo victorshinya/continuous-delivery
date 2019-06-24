18369f1779c1542414862b716c05a3033318ea0e
Victor <contato@victorshinya.com.br>
2019-06-24 19:31:42 -0300

    Add HTML file and MongoDB URI

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
 // https://github.com/expressjs/express
-const app = require('express')()
+const express = require('express')
+const app = express()
 // https://github.com/expressjs/body-parser
 const bodyParser = require('body-parser')
+// https://nodejs.org/api/path.html
+const path = require('path')
 // https://github.com/motdotla/dotenv
 require('dotenv').config()
 
@@ -23,6 +26,11 @@ require('./databases/mongodb')
  */
 const endpoints = require('./routes')
 app.use('/api', endpoints)
+
+/**
+ * Set public as default router
+ */
+app.use(express.static(path.join(__dirname, 'public')))
 
 /**
  * Define app port: VCAP/PORT dynamically port or a local (8080)
